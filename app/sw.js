82077ad400a3d8cdaff7ccf3da427dd132376b5c
tradingmaster5321-lang <tradingmaster5321@gmail.com>
2026-09-06 02:52:50 +0300

    Add Finesty Sales service worker

---
+++ app/sw.js
@@ -0,0 +1,5 @@
+const CACHE='finesty-sales-v2';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
